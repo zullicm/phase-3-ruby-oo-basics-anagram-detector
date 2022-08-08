b2f307e7866d57989c499c3115f514c638604a4a
Christopher Marzulli <chriszulli15@gmail.com>
2022-08-08 15:35:04 -0400

    commit tree 4887708725e448f72927337130f014d14fe4a60e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize word
+    @word = word
+  end
+  def match array
+    matching_words = array.filter do |word|
+      @word.chars.sort == word.chars.sort
+    end
+  end
+end
